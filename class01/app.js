dd72800a2cc9bea300e72cbc928c796b724b7fef
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-05-22 10:22:32 +0500

    Add files via upload

---
+++ class01/app.js
@@ -0,0 +1,27 @@
+// alert("Welcome to js class");
+
+//DataType
+//1-String "Hello World" 'hello world'
+//2-Number  25456
+//2-Boolean true/false
+
+//Variables
+// var message = "My name is Mark";
+// alert(message);
+
+
+// var message = "hello world";
+
+
+
+// message = "hello SMIT";
+// alert(message)
+
+
+// var message;
+// message = "hello world";
+// alert(message);
+
+///Question#01
+// alert("welcome")
+
