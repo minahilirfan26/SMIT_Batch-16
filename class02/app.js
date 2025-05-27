90bccfd41f2adf5f704c75a44d441f419b2b1df0
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-05-27 09:58:26 +0500

    Add files via upload

---
+++ class02/app.js
@@ -0,0 +1,51 @@
+//String 
+// var name = "Minahil Irfan";
+// name = "Minahil";
+// console.log(name);
+// document.write(name)
+
+///Number
+// var num = 20;
+// var num2 = 2;
+// var num1 = num2 + num;
+// document.write(num1);
+
+//Variable Names Legal
+// var name;
+// var myFullName;
+// var full_name;
+// var box1;
+// var box1hh;
+// var $name;
+
+//Variable Names IlLegal
+// var 45box;
+// var ful name;
+// var alret;
+// var var;
+
+
+// var num = 0;
+// alert(num);
+// num = num + 1; ///1
+// alert(num)
+// num = num + 1;
+// alert(num);
+
+// var num = 5;
+// alert(num);
+// num = num - 1;
+// alert(num);
+// num = num -1;
+// alert(num)
+
+var num = 5;
+alert(num);
+num--;
+alert(num);
+num--;
+alert(num);
+num--;
+alert(num);
+num--;
+alert(num);
