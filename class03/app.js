4180b4284d2694e869fffe314027401145550a4c
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-05-29 10:51:49 +0500

    Add files via upload

---
+++ class03/app.js
@@ -0,0 +1,72 @@
+// var num = 2;
+// console.log(num++);
+
+// var num  = 0;
+// alert(num);//0
+// num = num + 1;
+// alert(num);//1
+// num = num + 1;
+// alert(num);//2
+// num = num + 1;
+// alert(num);//3
+
+
+// var num  = 6;
+// alert(num);//6
+// num = num - 1;
+// alert(num);//5
+// num = num - 1;
+// alert(num);//4
+// num = num - 1;
+// alert(num);//3
+
+// var num = 0;
+// alert(num);
+// num++;//0 + 1
+// alert(num);
+// num++;//+1
+// alert(num);//2
+// num++;//+1
+// alert(num);//3
+// num++;
+// alert(num);
+
+
+///POST INCREMENT
+// var num = 0;
+// alert(num++);//0 
+// alert(num);
+
+//PRE-INCREMENT
+// var num = 7;
+// alert(--num);//0 
+// alert(num);
+
+
+// var a = 2;
+// var b = 3;
+// alert(a++ + ++a + --a + a--);
+//     //2   + //4 +  //3  *  3  
+
+// var a = 5;
+// alert(--a - ++a * a++ + ++a);
+//     //4   - //5 *  //5 +    //7
+
+// var a = 2;
+// var b = 3;
+
+// alert(a++ - ++b * (b-- + ++b) + ++a * --a);
+// //   2    -   4  *  ( 4   +   4) + 4  *  3
+
+
+// //Concatenating text strings
+// var name = "Minahil";
+// var fName = "Irfan";
+// alert(name + " " + fName);
+
+// var num1 = 4;
+// var num2 = 5;
+// alert(num1 + num2);
+
+
+
