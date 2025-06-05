faa3f029a0bf3ee15abf91c911e17a423a9edb15
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-06-05 10:43:03 +0500

    Add files via upload

---
+++ class05/app.js
@@ -0,0 +1,38 @@
+//ARRAY
+// var num = 88;
+// var str = "hello world";
+// var bool = true;
+
+// var arr = [23, "hello world", true, 78];
+// arr[4] = "SMIT";
+// console.log(arr);
+// console.log(arr[1])
+
+
+// var arr = [23, "hello world", true, 78];
+
+//ARRAy.pop
+// arr.pop();
+// console.log(arr)
+
+///Array.Push
+// arr.push("smit", "saylani");
+// console.log(arr)
+
+///ARRAY.shift
+// arr.shift();
+// console.log(arr)
+
+///ARRAY.unshift
+// arr.unshift("maham", "sadaf");
+// console.log(arr)
+
+//Splice
+// var arr = [23, "hello world", true, 78];
+// arr.splice(0,3,"SMIT", "Malir");
+// //(targetingIndex, removingVal, Adding)
+// console.log(arr)
+
+//Slice
+var arr = [23, "hello world", true, 78];
+console.log(arr.slice(1,3))
