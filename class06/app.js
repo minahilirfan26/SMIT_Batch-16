cba4bad1fb7f2e2a4517d28b3449ce2dbc3612f2
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-06-10 10:58:12 +0500

    Add files via upload

---
+++ class06/app.js
@@ -0,0 +1,22 @@
+// var ar = [];
+// ar[0] = "Minahil";
+// ar[1] = "SMIt ghoar";
+// ar[2] = "WMA";
+// console.log(ar[1])
+// var arr = ["SMIT", 65, true, "tuesday"];
+// arr = "Saylani";
+// arr.push("Saylani")
+// console.log(arr.shift())
+// console.log(arr.unshift("ayesha"))
+// console.log(arr)
+
+// arr.splice(1,2, "Saylani mass it");
+
+// console.log(arr.slice(1,3))
+
+
+//For Loop
+var arr = ["mon", "tues", "wed", "thurs", "fri"]
+for(var i = 0; i < arr.length; i++){
+    console.log(i ,arr[i]);
+}
