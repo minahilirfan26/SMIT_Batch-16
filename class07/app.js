cf1b22df6691869e133c8179339d7d3c690e484a
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-06-14 10:23:39 +0500

    Add files via upload

---
+++ class07/app.js
@@ -0,0 +1,42 @@
+// for(var i = 0; i < 5; i++){
+//     console.log(i)
+//   if(i === 3){
+//     break;
+//   }
+// }
+
+// var arr = ["apple", "mango", "lychee", "kiwi"];
+// var fruit = ["Fruit"]
+// for(var i = 0; i < arr.length; i++){
+//    for(var j = 0; j < fruit.length; j++){
+//     console.log(arr[i] , fruit[j])
+//    }
+// };
+
+
+// for(var i = 0; i <= 5; i++){
+//     for(var j = 1; j <=5; j++){
+//         console.log(i + " " + j);
+//     }
+// }
+// 0 1
+// 0 2
+// 0 3
+// 0 4
+// 0 5
+// 1 1
+// 1 2
+// 1 3
+
+
+// for( var i = 5; i >= 0; i--){
+//     console.log(i)
+// }
+
+// var str = "hello world";
+// console.log(str.toUpperCase())
+
+var city = prompt("enter your city");
+if(city.toUpperCase() === "KARACHI"){
+    console.log("welcome to the city of lights", city);
+}
