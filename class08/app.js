66cb00aa835dc89f34ec8d86f07602628439ae45
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-06-21 10:33:28 +0500

    Add files via upload

---
+++ class08/app.js
@@ -0,0 +1,41 @@
+// var eng = +prompt("enter eng subj num");
+// var urdu = +prompt("enter urdu subj num");
+// var math = +prompt("enter math subj num");
+// var obtNum = eng+urdu+math;
+// var totalNum = 300;
+
+// document.write("English:" + eng + '<br>');
+// document.write("Urdu:" + urdu + '<br>');
+// document.write("Math:" + math + '<br>');
+// document.write("Obt Num:" + obtNum);
+// document.write(obtNum*100 / totalNum + "%");
+
+
+
+// var str = "minahil irfan shaikh";
+// var arr = ["mon", "tues", "sat"]
+
+// var word = arr.indexOf("sun");
+// console.log(str.charAt(7));
+
+// var word = str.replace("world", "class")
+// console.log(word)
+// console.log(str)
+
+// var str = prompt("enter your name");
+// //minahil irfan shaikh
+// var word = str.split(" ");
+// var arr = [];
+// //minahil
+// // console.log(word)
+// for(var i = 0; i < word.length; i++){
+//     // console.log(word[i])
+//     var caplitalizedWord = word[i].charAt(0).toUpperCase() + word[i].slice(1);
+     
+//         arr.push(caplitalizedWord)
+          
+
+// }
+
+// var finalResult = arr.join(" ");
+// console.log(typeof(finalResult) + finalResult)
