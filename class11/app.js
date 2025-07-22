d8beae93eeb912258995f3dd4021f592a6a7b251
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-07-22 10:42:33 +0500

    Add files via upload

---
+++ class11/app.js
@@ -0,0 +1,91 @@
+// var obj = {
+//     name: "Minahil",
+//     email: "duashaikh603@gmail.com",
+//     age: 22,
+//     active: true,
+//     days: ["tues", "thurs", "sat"],
+//     courseDetails: {
+//         campus: "karachi",
+//         location: "Malir"
+//     }
+// }
+
+// delete obj.email;
+// obj.courseDetails.campus = "SMIT";
+// console.log(obj)
+
+// var obj = {
+//     email: "duashaikh603@gmail.com",
+//     name: "Minahil",
+//     fName: "Irfan", 
+//     getDetails: function(){
+//        console.log(this)
+//     }
+// };
+
+// obj.getDetails()
+// console.log()
+
+// function abc(){
+// console.log(this)
+// }
+
+// abc()
+//  console.log(this)
+
+// function Students(name, email, age){
+// this.name = name;
+// this.email = email;
+// this.age = age
+// }
+
+// var stdDetails = [
+//     new Students("minahil", "duashaikh603@gmail.com", 22),
+//     new Students("Maham", "maha@gmail.com", 22),
+//     new Students("iqra", "irqa@gmail.com", 22),
+//     new Students("rida", "rida@gmail.com", 22)
+// ]
+// console.log(stdDetails)
+
+
+// var students = [
+//     {
+//         name: "rida",
+//         email: "rida@gmail.com",
+//         img: 'https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D'
+//     },
+//     {
+//         name: "iqra",
+//         email: "rida@gmail.com"
+//     },
+//     {
+//         name: "iqra",
+//         email: "rida@gmail.com"
+//     },
+//     {
+//         name: "iqra",
+//         email: "rida@gmail.com"
+//     }
+// ]
+
+
+// var cloths = {
+//     men: {
+//         shirt: {
+
+//         }
+//     },
+//     womens: {
+//         froks: {
+//             longFroks: {
+
+//             },
+//             shortFroks: {
+
+//             },
+//             maxis: {
+
+//             }
+//         }
+//     }
+// }
