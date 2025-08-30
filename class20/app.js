681106edbe06da505a41f09eb675d19088bf2699
Minahil Irfan <99713184+minahilirfan26@users.noreply.github.com>
2025-08-30 10:48:28 +0500

    Add files via upload

---
+++ class20/app.js
@@ -0,0 +1,59 @@
+// function abc(cb){
+// setTimeout(()=>{
+//     console.log("completed")
+//     cb()
+// },3000)
+// }
+
+// function cb(){
+//     console.log("done")
+// }
+// abc(cb)
+
+
+// function abc(xyz){
+// return ()=>{
+
+// xyz()
+// }
+// }
+
+// function xyz(){
+
+// }
+// abc(xyz)
+
+
+// let age = 0
+
+// // let result = age >= 20 ? "allow" : "not allow";
+// let result = age && "allow"
+// console.log(result)
+
+// export default  ()=>{
+// return new Date().toDateString()
+// }
+// let fullName = "Minahil irfan"
+// export { fullName}
+// export default date
+
+
+// let abc = a=> a + 20 ;
+
+// console.log(abc(2))
+
+// let obj = {
+//     name: "minahil",
+//     email: "duashailkj603@gmail.com",
+//     getDetails: ()=>{
+//         console.log(this)
+//     }
+// }
+// obj.getDetails()
+// console.log()
+
+// console.log(abc())
+
+// let abc = ()=>{
+//     return "hello"
+// }
---
+++ class20/index.js
@@ -0,0 +1,6 @@
+// import { fullName as myName } from './app.js'
+// import currentDat from './app.js'
+
+// let fullName = "Maham"
+// console.log(currentDat())
+
